3e8e9953bd1b7215389c248b32d49778bfd23aef
Sushan Achar <sushanachar2006@gmail.com>
2025-06-11 15:26:35 +0530

    Update pattern7.cpp

--- pattern7.cpp
+++ pattern7.cpp
@@ -8,9 +8,9 @@ void pattern7(int n){
         for(int j=0;j<2*i+1;j++){
             cout<<" *";
         }
-        // for(int j=0;j<n-i-1;j++){
-        //    cout<<" "; 
-        // }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" "; 
+        }
         cout<< endl;
     }
 }
